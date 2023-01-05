5fd1eb1e9708853d4fa7b15f6940f8c7ef23c3a9
Allyson Mitchem <allysonmitchem@gmail.com>
2023-01-05 15:15:30 -0700

    home and product components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-
+import Home from './Home';
 import Header from "./Header"
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Home/> */}
+      <Home/>
     </div>
   );
 }
